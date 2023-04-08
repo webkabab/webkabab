0bfee2945ea49b22e7092b388f4fbfd3e1282ade
webkabab <47575744+webkabab@users.noreply.github.com>
2023-04-08 18:09:50 +0300

    .

--- WebKabab.js
+++ WebKabab.js
@@ -561,7 +561,7 @@ function extractSdarotVideo(series, season, episode, token, onSuccess, onError) 
     // TODO: Update those
     let REMEMBER_COOKIE = "remember=a%3A2%3A%7Bs%3A8%3A%22username%22%3Bs%3A7%3A%22sheldom%22%3Bs%3A5%3A%22check%22%3Bs%3A128%3A%22484d953e6d3b040ed93c4168d88633a4aa6288ea3afccfb9db8595f46e6b5e43e780de14da73e5684c2ea9e5e3e36f77298a9590e95eb86794c0d5d279b48dbb%22%3B%7D";
     let SDAROT_COOKIE = "Sdarot=IMluGd3P5kiHeBoKFLsNhQ4m2aRmtrEuuMYpE3XwS5LQMOpBZ9Wcvg%2CXzXz64dCJ7IKP3Gefjcx6kVxzdLQT-%2C-%2C%2CrwZ1Ff%2CbrUMpYPdoNQJ%2CuTkxfahUFcrDswcRwHJ";
-    headers["Cookie"] =  REMEMBER_COOKIE; + "; " + SDAROT_COOKIE; // TODO: debug
+    headers["Cookie"] =  REMEMBER_COOKIE + "; " + SDAROT_COOKIE; // TODO: debug
 
     let params = {};
     params["SID"] = String(series);
