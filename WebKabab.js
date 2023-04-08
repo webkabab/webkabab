b55447a8152357bdaff45fb38eab33433abac4de
webkabab <47575744+webkabab@users.noreply.github.com>
2023-04-09 01:03:52 +0300

    .

--- WebKabab.js
+++ WebKabab.js
@@ -652,7 +652,7 @@ function searchSdarot(req, query) {
     let headers = getSdarotHeaders();
     let params = {};
     params["search"] = query;
-    let searchResults = TiviProvider.sendHTTPRequest(req, SEARCH_API, "GET", headers, params, true);
+    let searchResults = TiviProvider.sendHTTPRequest(req, "GET", SEARCH_API , headers, params, true);
     console.log("Got search results for q="+query+": "+searchResults);
     if(searchResults) {
         searchResults = JSON.parse(searchResults);
