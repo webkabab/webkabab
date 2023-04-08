ba7c954bc9696ff9c7bb84c49551416dd69a34b4
webkabab <47575744+webkabab@users.noreply.github.com>
2023-04-08 17:43:29 +0300

    .

--- WebKabab.js
+++ WebKabab.js
@@ -574,7 +574,7 @@ function extractSdarotVideo(series, season, episode, token, onSuccess, onError) 
     let getToken = function() {
 
         // First, visit the main site to set an up-to-date cookie
-        sendHTTPRequest(req, BASE_SITE, "GET", {}, {}, false);
+        sendHTTPRequest(req, BASE_SITE, "GET", headers, {}, false);
 
         params = {};
         params["SID"] = String(series);
