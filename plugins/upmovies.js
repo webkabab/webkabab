840b207cc6609f628dbfa81dd6e9d382288292a5
webkabab <47575744+webkabab@users.noreply.github.com>
2024-08-18 10:13:26 +0300

    .

--- plugins/upmovies.js
+++ plugins/upmovies.js
@@ -186,7 +186,8 @@ function searchUpMovies(req, query) {
                 while(match = itemRegex.exec(searchResults)) {
                     let url = match[1];
                     let name = match[2];    
-                    name = name.replaceAll("+", " ");
+                    //name = name.replaceAll("+", " ");
+                    name = name.replace(/[+]/g, " ")
                     console.debug("Got title: "+name+" url="+url);
                     let parts = url.split("/");
                     let id = parts[parts.length - 1];
