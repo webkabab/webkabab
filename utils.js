86e65bdaa9e5421014f9a041ac0217fb0fe9f0bd
webkabab <47575744+webkabab@users.noreply.github.com>
2024-08-19 20:48:42 +0300

    .

--- utils.js
+++ utils.js
@@ -223,7 +223,7 @@ function stringSimilarity(str1, str2) {
     const normalizedString1 = str1.toLowerCase();
     const normalizedString2 = str2.toLowerCase(); 
 
-    const distance = module.exports.Levenshtein.get(normalizedString1, normalizedString2);
+    const distance = Levenshtein.get(normalizedString1, normalizedString2);
     const maxLength = Math.max(normalizedString1.length, normalizedString2.length);
     return 1 - (distance / maxLength);
 }
